a02534d103eac858fc6ff6781d22eaa9320e0eda
Sam Bersch <sammybersch@gmail.com>
2025-06-05 15:09:31 -0400

    added zellij status bar

--- zsh/.zshrc.d/zellij.sh
+++ zsh/.zshrc.d/zellij.sh
@@ -4,14 +4,12 @@ function zj() {
         return
     fi
 
-    max=$(zellij ls -s | sort -r | cut -d "-" -f 2 | head -n 1)
+    max=$(zellij ls -s | sort -hr | cut -d "-" -f 2 | head -n 1)
     if [[ -z $max ]] || [[ $max == "No active sessions found" ]]; then
         max=0
     fi
 
-    echo "we got" $max
     max=$(($max + 1))
-    echo $max
     user=$(whoami)
 
     zellij -s "${user}-${max}"
